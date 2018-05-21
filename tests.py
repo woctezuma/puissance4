865cb8712cc514bfb47051aed809c45fc8f902fe
Wok <woctezuma@users.noreply.github.com>
2018-05-21 12:45:01 +0200

    Add unit test

--- tests.py
+++ tests.py
@@ -5,9 +5,11 @@ import main
 
 class TestMainMethods(unittest.TestCase):
 
-    def test_main(self):
+    def test_no_player_for_a_single_game(self):
         self.assertTrue(main.menu('0'))
 
+    def test_no_player_for_many_games(self):
+        self.assertTrue(main.menu('3'))
 
 if __name__ == '__main__':
     unittest.main()
