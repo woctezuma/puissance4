3b173727620edde32b03a468ad13431d00db6c6c
Wok <woctezuma@users.noreply.github.com>
2018-05-22 10:00:32 +0200

    Improve reference to StackOverflow for tree structure

--- node.py
+++ node.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-
-# Source directe :
-# http://stackoverflow.com/questions/3753665/python-tree-structure-and-numerical-codes/3753796#3753796
-
-
 class Node(object):
-    """Structure d'arbre"""
+    """
+    Tree structure
+
+    Reference: http://stackoverflow.com/questions/3753665/python-tree-structure-and-numerical-codes/3753796#3753796
+    """
 
     byname = {}
 
     def __init__(self, name, parent=None):
-        """Créer un noeud, en spécifiant son parent s'il existe"""
         self.name = name
         self.parent = parent
         self.children = []
