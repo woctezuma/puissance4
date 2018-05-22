99a095e7fee37cc485bd906e4e499da4b643fdbc
Wok <woctezuma@users.noreply.github.com>
2018-05-22 16:33:01 +0200

    Refactor main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from time import sleep
-
 from grille import Grille
 from mc import MC
 from uct import UCT
 from utils import get_possible_player_inputs, convert_player_input
 
 
-def menu(default_user_action=None):
+def menu(default_user_action=None, num_parties_jouees=50):
     """Menu principal"""
     user_input = []
     while user_input not in ['q']:
@@ -49,11 +47,9 @@ def menu(default_user_action=None):
             player_O = MC('O')
             player_O.set_params(player_O.get_default_params())
 
-            num_parties_jouees = 50
+            ratio_victoires = analyze_AI_self_plays(player_X, player_O, num_parties_jouees)
 
-            ratio_victoires = obtenir_statistiques_de_victoires(player_X, player_O, num_parties_jouees)
-
-            print("[Players X vs. O] winrate = {:.2f} (over {} games)".format(ratio_victoires, num_parties_jouees))
+            print("\n[Players X vs. O] winrate = {:.2f} (over {} games)".format(ratio_victoires, num_parties_jouees))
 
             player_X.print()
             player_O.print()
@@ -64,77 +60,100 @@ def menu(default_user_action=None):
     return True
 
 
-def np(player_X, player_O, show_grid=False, verbose=True):
+def np(ai_player_X, ai_player_O, verbose=True):
     """Une partie entre intelligences artificielles"""
-    grid = Grille()
+    grille = Grille()
+    player_who_last_played = None
 
-    # TODO
+    step_counter = 0
+    while not (grille.check_victory()) and len(grille.look_for_allowed_steps()) > 0:
 
-    le_joueur1_gagne = False
-    mes_coups_possibles = grid.look_for_allowed_steps()
-    while grid.check_victory() is False and len(mes_coups_possibles) > 0:
-        mon_coup = player_X.play_with_bias(grid)
-        grid.drop(player_X.player, mon_coup)
-        le_joueur1_gagne = True
-        mes_coups_possibles = grid.look_for_allowed_steps()
-        if show_grid:
-            grid.show_grid()
-            sleep(1)
-        if grid.check_victory() is False and len(mes_coups_possibles) > 0:
-            votre_coup = player_O.play_with_bias(grid)
-            grid.drop(player_O.player, votre_coup)
-            le_joueur1_gagne = False
-            mes_coups_possibles = grid.look_for_allowed_steps()
-            if show_grid:
-                grid.show_grid()
-    il_y_a_un_vainqueur = grid.check_victory()
-    num_steps = grid.get_num_steps()
+        if step_counter % 2 == 0:
+            current_player = ai_player_X
+        else:
+            current_player = ai_player_O
+
+        grille.drop(current_player.get_player_symbol(), current_player.play_with_bias(grille))
+        player_who_last_played = current_player.get_player_symbol()
+
+        step_counter += 1
+
+    if grille.check_victory():
+        winner_symbol = player_who_last_played
+    else:
+        winner_symbol = 'draw'
+
+    num_steps = grille.get_num_steps()
 
     if verbose:
-        print_victory_screen(grid)
+        print_victory_screen(grille, winner_symbol)
 
-    return il_y_a_un_vainqueur, le_joueur1_gagne, num_steps
+    return winner_symbol, num_steps
 
 
-def sp(player_O):
+def sp(ai_player):
     """Une partie opposant un joueur humain à une intelligence artificielle"""
-    grid = Grille()
+    grille = Grille()
+    player_who_last_played = None
 
-    # TODO
+    human_player = 'X'
+    ai_player.player = 'O'
 
-    mes_coups_possibles = grid.look_for_allowed_steps()
     user_input = []
-    player = 'X'
-    while user_input != 'q' and grid.check_victory() is False and len(mes_coups_possibles) > 0:
-        grid.show_grid()
-        user_input = input("Letter a through g  = drop disc, q = quit. \nYour move:")
-        if user_input in get_possible_player_inputs(grid.width):
-            mon_coup = convert_player_input(user_input)
-            if grid.drop(player, mon_coup):
 
-                mes_coups_possibles = grid.look_for_allowed_steps()
-                if grid.check_victory() is False and len(mes_coups_possibles) > 0:
-                    votre_coup = player_O.play_with_bias(grid)
-                    grid.drop(player_O.player, votre_coup)
+    step_counter = 0
+    while user_input not in ['q'] and not (grille.check_victory()) and len(grille.look_for_allowed_steps()) > 0:
 
-                    mes_coups_possibles = grid.look_for_allowed_steps()
+        if step_counter % 2 == 0:
+            current_player = human_player
 
-    print_victory_screen(grid)
-    return
+            grille.show_grid()
+
+            while True:
+                user_input = input("Letter a through g  = drop disc, q = quit. \nYour move:")
+                if user_input in get_possible_player_inputs(grille.width):
+                    mon_coup = convert_player_input(user_input)
+                    if grille.drop(current_player, mon_coup):
+                        break
+
+            player_who_last_played = human_player
+
+        else:
+            current_player = ai_player
+
+            grille.drop(current_player.get_player_symbol(), current_player.play_with_bias(grille))
+
+            player_who_last_played = current_player.get_player_symbol()
+
+        step_counter += 1
+
+    if grille.check_victory():
+        winner_symbol = player_who_last_played
+    else:
+        winner_symbol = 'draw'
+
+    num_steps = grille.get_num_steps()
+
+    print_victory_screen(grille, winner_symbol)
+
+    return winner_symbol, num_steps
 
 
 def pvp():
     """Une partie entre joueurs humains"""
     grid = Grille()
+
     user_input = []
     player = 'X'
     last_player = None
-    while user_input != 'q' and not (grid.check_victory()):
+
+    while user_input not in ['q'] and not (grid.check_victory()):
         grid.show_grid()
         user_input = input("Letter a through g = drop disc, q = quit. \nPlayer " + player + " to move:")
         if user_input in get_possible_player_inputs(grid.width):
             mon_coup = convert_player_input(user_input)
             last_player = player
+
             if grid.drop(player, mon_coup):
                 if player == 'X':
                     player = 'O'
@@ -151,35 +170,32 @@ def print_victory_screen(grid, winner_symbol='draw'):
         if winner_symbol != 'draw':
             print("Player {} wins. ".format(winner_symbol))
         else:
-            print("This is a draw. ".format(winner_symbol))
+            print("This ends with a draw. ".format(winner_symbol))
 
     return
 
 
-def obtenir_statistiques_de_victoires(player_X, player_O, num_parties_jouees):
-    # TODO
+def analyze_AI_self_plays(ai_player_X, ai_player_O, num_parties_jouees):
+    num_victoires = {'O': 0, 'X': 0, 'draw': 0}
 
-    num_parties_avec_vainqueur = 0
-    num_victoires_du_joueur1 = 0
     for game_no in range(num_parties_jouees):
-        (il_y_a_un_vainqueur, le_joueur1_gagne, num_steps) = np(player_X, player_O, verbose=False)
-        num_parties_avec_vainqueur += int(il_y_a_un_vainqueur)
-        num_victoires_du_joueur1 += int(le_joueur1_gagne)
+        winner_symbol, num_steps = np(ai_player_X, ai_player_O, verbose=False)
         print('Game n°{}\tnum_steps={}'.format(game_no + 1, num_steps))
+        num_victoires[winner_symbol] += 1
+
     try:
-        ratio_victoires = num_victoires_du_joueur1 / num_parties_avec_vainqueur
+        ratio_victoires = num_victoires['X'] / (num_victoires['X'] + num_victoires['O'])
     except ZeroDivisionError:
         ratio_victoires = 0.5
+
     return ratio_victoires
 
 
 if __name__ == "__main__":
-    test_user_action = '3'
     # Set to None if you want to decide at run-time which kind of play you prefer (AI self-play, human vs. AI, etc.)
     # Set to '0' to let AI vs. AI self-play (useful for Travis integration on Github)
-    # Set to '1' to play one game against biased Monte-Carlo AI.
-    # NB: This should not be the best AI. Modify the code if you want to play against UCT AI.
+    # Set to '1' to play one game against UCT AI.
     # Set to '2' to allow with no AI, i.e. human vs. human.
     # Set to '3' to compute AI self-play 50 games: UCT AI vs. biased Monte-Carlo AI.
 
-    menu(test_user_action)
+    menu(default_user_action='3', num_parties_jouees=5)
