f7e7802e3f3665328ddd5b3f0b6a7ae2eb43adb5
Wok <woctezuma@users.noreply.github.com>
2018-05-22 16:37:25 +0200

    Deactivate byname in Node class

--- node.py
+++ node.py
@@ -5,13 +5,13 @@ class Node(object):
     Reference: http://stackoverflow.com/questions/3753665/python-tree-structure-and-numerical-codes/3753796#3753796
     """
 
-    byname = {}
+    # byname = {}
 
     def __init__(self, name, parent=None):
         self.name = name
         self.parent = parent
         self.children = []
-        self.byname[name] = self
+        # self.byname[name] = self
         if parent is None:  # root pseudo-node
             self.code = 0
         else:  # all normal nodes
