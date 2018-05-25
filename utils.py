bc9cc335825685d7e7af52d376e540b3b874e944
Wok <woctezuma@users.noreply.github.com>
2018-05-25 08:57:29 +0200

    Default paramteters

--- utils.py
+++ utils.py
@@ -10,16 +10,23 @@ def convert_player_input(player_input):
     return ord(player_input) - get_conversion_offset()
 
 
-def get_default_mc_params():
+def get_default_ai_params():
     params = dict()
+    params['bias_to_obvious_steps'] = True
+    params['max_num_steps_to_explore'] = None
+
+    return params
+
+
+def get_default_mc_params():
+    params = get_default_ai_params()
     params['num_tirages_MC'] = 3
 
     return params
 
 
 def get_default_uct_params():
-    params = dict()
-    params['num_tirages_MC'] = 3
+    params = get_default_mc_params()
     params['num_descentes_dans_arbre'] = 7
     params['facteur_uct'] = 0.0
 
