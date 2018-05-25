40439e0255479bd36694f38eef4008595dd87f50
Wok <woctezuma@users.noreply.github.com>
2018-05-25 20:00:20 +0200

    Unit test for training module

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 import main
+import training
 
 
 class TestMainMethods(unittest.TestCase):
@@ -9,7 +10,13 @@ class TestMainMethods(unittest.TestCase):
         self.assertTrue(main.menu('0'))
 
     def test_no_player_for_many_games(self):
-        self.assertTrue(main.menu('3'))
+        self.assertTrue(main.menu('3', num_parties_jouees=3))
+
+
+class TestTrainingMethods(unittest.TestCase):
+
+    def test_prepare_and_train(self):
+        self.assertTrue(training.prepare_and_train(num_parties_jouees=3))
 
 
 if __name__ == '__main__':
