ace0545b87d3e5726f6d9ead313c0c29993a0416
Wok <woctezuma@users.noreply.github.com>
2018-05-25 12:21:09 +0200

    Document trick parameters ; Update default parameter values

--- parameters.py
+++ parameters.py
@@ -22,24 +22,40 @@ def get_default_max_num_steps_to_explore():
 # noinspection PyPep8Naming
 def get_default_num_tirages_MC():
     # noinspection PyPep8Naming
-    num_tirages_MC = 3
+    num_tirages_MC = 14
     return num_tirages_MC
 
 
 # Only for UCT AI
 
 def get_default_num_descentes_dans_arbre():
-    num_descentes_dans_arbre = 7
+    """
+    UCT algorithm relies on Monte-Carlo Tree search. However, it differs from MCT because it works in a layered manner.
+
+        repeat 'num_descentes_dans_arbre' times the following procedure:
+            - Tree Down: a node is reached based on UCT search criteria,
+            - Monte Carlo: the node board state is evaluated using num_tirages_MC samples,
+            - Tree Up: the evaluation is propagated up in the search tree using UCT update rules for evaluations.
+
+    """
+    num_descentes_dans_arbre = 14
     return num_descentes_dans_arbre
 
 
 def get_default_facteur_uct():
-    facteur_uct = 0.0
+    """
+    The following UCT parameter could be equal to 0.
+    However, to take full advantage of the UCT algorithm, use a positive (>0) value.
+    """
+    facteur_uct = 0.1
     return facteur_uct
 
 
 # For main
 
 def get_default_num_parties_jouees():
+    """
+    The number of AI self-play games to find out which of two AI is the best one.
+    """
     num_parties_jouees = 50
     return num_parties_jouees
