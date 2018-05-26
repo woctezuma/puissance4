5a9ea5a462b56cfe45c3c2c773c546c66cc93834
Wok <woctezuma@users.noreply.github.com>
2018-05-26 09:28:33 +0200

    Display helper

--- utils.py
+++ utils.py
@@ -2,8 +2,11 @@ def get_conversion_offset():
     return 97
 
 
+def convert_to_column_display(col):
+    return chr(col + get_conversion_offset())
+
 def get_possible_player_inputs(width):
-    return [chr(i + get_conversion_offset()) for i in range(width)]
+    return [convert_to_column_display(i) for i in range(width)]
 
 
 def convert_player_input(player_input):
