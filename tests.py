4cfe5064008112b91e6bd012d93aa7a9139ac6e5
Wok <woctezuma@users.noreply.github.com>
2018-05-26 15:20:00 +0200

    Remove unit test for main.py

--- tests.py
+++ tests.py
@@ -1,16 +1,6 @@
 import unittest
 
-import main
 import training
-
-
-class TestMainMethods(unittest.TestCase):
-
-    def test_no_player_for_a_single_game(self):
-        self.assertTrue(main.menu('0'))
-
-    def test_no_player_for_many_games(self):
-        self.assertTrue(main.menu('3', num_parties_jouees=3))
 
 
 class TestTrainingMethods(unittest.TestCase):
