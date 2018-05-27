738aaf7a6e5f0d948777bd7705142348478863a1
Wok <woctezuma@users.noreply.github.com>
2018-05-27 11:34:01 +0200

    Unit test: import puissance4 package

--- tests/tests_puissance4.py
+++ tests/tests_puissance4.py
@@ -1,13 +1,13 @@
 import unittest
 
-from puissance4 import training
+import puissance4
 
 
 class TestTrainingMethods(unittest.TestCase):
 
     def test_prepare_and_train(self):
-        is_consistent, num_victories, num_steps = training.prepare_and_train(trainer_choice='Random',
-                                                                             num_parties_jouees=200)
+        is_consistent, num_victories, num_steps = puissance4.prepare_and_train(trainer_choice='Random',
+                                                                               num_parties_jouees=200)
         self.assertTrue(is_consistent)
 
 
